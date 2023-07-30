f086497c4a2c9205ddc71390242f519ca86991b3
ihorvasiuta <i.r.vasiuta@gmail.com>
2023-07-30 21:44:22 +0200

    add winston and stored&connected data in PG

--- index.ts
+++ index.ts
@@ -1,84 +1,14 @@
 // index.ts
 
-// const request = require('request');
-
-// const options = {
-//   method: 'POST',
-//   url: 'https://lastpass.com/enterpriseapi.php',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     "cid": 26272911,
-//     "provhash": "3d04fc60d502489779c5bad0329e0a996255850ee9c2e42e85eeb2b7d5ec47eb",
-//     "cmd": "getuserdata",
-//     "data": {
-//         "username": "testing.akk32@gmail.com"
-//     }
-//   })
-// };
-
-// request(options, function (error, response, body) {
-//   if (error) throw new Error(error);
-
-//   console.log(body);
-// });
-
-
-// const request = require('request');
-
-// async function getLastPassUserData(username) {
-//   const options = {
-//     method: 'POST',
-//     url: 'https://lastpass.com/enterpriseapi.php',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       "cid": 26272911,
-//       "provhash": "3d04fc60d502489779c5bad0329e0a996255850ee9c2e42e85eeb2b7d5ec47eb",
-//       "cmd": "getuserdata",
-//       "data": {
-//         "username": username
-//       }
-//     })
-//   };
-
-//   try {
-//     const response = await new Promise((resolve, reject) => {
-//       request(options, function (error, response, body) {
-//         if (error) {
-//           console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
-//           reject(error);
-//         }
-//         resolve(JSON.parse(body));
-//       });
-//     });
-//     console.log(`Successfully retrieved data for user ${username}`);
-//     return response;
-//   } catch (error) {
-//     console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
-//     throw error;
-//   }
-// }
-
-// async function main() {
-//   try {
-//     const userData = await getLastPassUserData("testing.akk32@gmail.com");
-//     console.log(userData);
-//   } catch (error) {
-//     console.error(`An error occurred while running the script: ${error}`);
-//   }
-// }
-
-// main();
 
 
 
 
 
+//with PG
 const request = require('request');
 const winston = require('winston');
+const { Client } = require('pg');
 
 const logger = winston.createLogger({
   level: 'info',
@@ -86,6 +16,15 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console()
   ]
+});
+
+// Update these values to match your database setup
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "0000", 
+  database: "mainlastpassintegration"
 });
 
 async function getLastPassUserData(username) {
@@ -123,10 +62,25 @@ async function getLastPassUserData(username) {
   }
 }
 
+async function storeUserDataInDatabase(userData) {
+  try {
+    await client.connect();
+    const query = `INSERT INTO maintable (last_login, sites, last_pw_change) VALUES ($1, $2, $3)`;
+    const values = [userData.last_login, userData.sites, userData.last_pw_change];
+    await client.query(query, values);
+    logger.info(`Successfully stored data in database`);
+  } catch (error) {
+    logger.error(`An error occurred while storing data in the database: ${error}`);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
 async function main() {
   try {
     const userData = await getLastPassUserData("testing.akk32@gmail.com");
-    console.log(userData);
+    await storeUserDataInDatabase(userData);
   } catch (error) {
     logger.error(`An error occurred while running the script: ${error}`);
   }
