af9d64c31bc88986719f78eb662824dea05d866f
ihorvasiuta <i.r.vasiuta@gmail.com>
2023-07-30 19:05:34 +0200

    add winston logger

--- index.ts
+++ index.ts
@@ -1,43 +1,4 @@
 // index.ts
-// require('dotenv').config();
-// const request = require('request');
-
-// async function getLastPassUserData(username: string) {
-//   const options = {
-//     method: 'POST',
-//     url: 'https://lastpass.com/enterpriseapi.php',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       "cid": 26272911,
-//       "provhash": "3d04fc60d502489779c5bad0329e0a996255850ee9c2e42e85eeb2b7d5ec47eb",
-//       "cmd": "getuserdata",
-//       "data": {
-//         "username": "testing.akk32@gmail.com"
-//       }
-//     })
-//   };
-
-//   try {
-//     const response = await new Promise((resolve, reject) => {
-//       request(options, function (error, response, body) {
-//         if (error) reject(error);
-//         resolve(body);
-//       });
-//     });
-//     console.log(response);
-//   } catch (error) {
-//     console.error(`An error occurred while retrieving data from the LastPass API: ${error}`);
-//   }
-// }
-
-// getLastPassUserData("testing.akk32@gmail.com");
-
-
-
-
-
 
 // const request = require('request');
 
@@ -64,7 +25,68 @@
 // });
 
 
+// const request = require('request');
+
+// async function getLastPassUserData(username) {
+//   const options = {
+//     method: 'POST',
+//     url: 'https://lastpass.com/enterpriseapi.php',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       "cid": 26272911,
+//       "provhash": "3d04fc60d502489779c5bad0329e0a996255850ee9c2e42e85eeb2b7d5ec47eb",
+//       "cmd": "getuserdata",
+//       "data": {
+//         "username": username
+//       }
+//     })
+//   };
+
+//   try {
+//     const response = await new Promise((resolve, reject) => {
+//       request(options, function (error, response, body) {
+//         if (error) {
+//           console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
+//           reject(error);
+//         }
+//         resolve(JSON.parse(body));
+//       });
+//     });
+//     console.log(`Successfully retrieved data for user ${username}`);
+//     return response;
+//   } catch (error) {
+//     console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
+//     throw error;
+//   }
+// }
+
+// async function main() {
+//   try {
+//     const userData = await getLastPassUserData("testing.akk32@gmail.com");
+//     console.log(userData);
+//   } catch (error) {
+//     console.error(`An error occurred while running the script: ${error}`);
+//   }
+// }
+
+// main();
+
+
+
+
+
 const request = require('request');
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
 
 async function getLastPassUserData(username) {
   const options = {
@@ -87,16 +109,16 @@ async function getLastPassUserData(username) {
     const response = await new Promise((resolve, reject) => {
       request(options, function (error, response, body) {
         if (error) {
-          console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
+          logger.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
           reject(error);
         }
         resolve(JSON.parse(body));
       });
     });
-    console.log(`Successfully retrieved data for user ${username}`);
+    logger.info(`Successfully retrieved data for user ${username}`);
     return response;
   } catch (error) {
-    console.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
+    logger.error(`An error occurred while retrieving data from the LastPass API for user ${username}: ${error}`);
     throw error;
   }
 }
@@ -106,9 +128,8 @@ async function main() {
     const userData = await getLastPassUserData("testing.akk32@gmail.com");
     console.log(userData);
   } catch (error) {
-    console.error(`An error occurred while running the script: ${error}`);
+    logger.error(`An error occurred while running the script: ${error}`);
   }
 }
 
 main();
-
